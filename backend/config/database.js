2aea67de37967bd02693bc830bcbb19b9654c101
unknown <Kamilakhtartanoli@gmail.com>
2025-12-23 23:33:22 +0500

    11:33

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
 
-const database = async (req,res) => {
+const database = async (req, res) => {
     const url = process.env.databaselink
     try {
-        const response = await mongoose.connect(url)
+        const response = await mongoose.connect(url, {
+            serverSelectionTimeoutMS: 30000 // ✅ added (30 seconds)
+        })
         console.log(`database-connected-successfully`)
     } catch (error) {
         console.log(`database-not-connected ${error.message}`)
     }
 }
 
-module.exports = {database}
+module.exports = { database }
